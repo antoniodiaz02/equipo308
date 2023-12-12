5e3d3e3ddf2f5d9b50d4b2479c34e224bf1c8d5c
Antonio Diaz <mesidiez@gmail.com>
2023-12-12 21:23:03 +0100

    Añadiendo prototipo main

---
+++ main.cc
@@ -0,0 +1,132 @@
+#include <iostream>
+
+using namespace std;
+
+void menuOrganizador() {
+    int opcionOrganizador;
+
+    cout << "Menú Organizador:" << endl;
+    cout << "1. Crear actividad académica" << endl;
+    cout << "2. Eliminar actividad académica" << endl;
+    cout << "3. Modificar actividad académica" << endl;
+    cout << "4. Asignar actividad académica a un director" << endl;
+    cout << "5. Cambiar rol a alguien" << endl;
+    cout << "6. Crear mailing" << endl;
+    cout << "7. Borrar mailing" << endl;
+    cout << "Ingrese el número correspondiente a su elección: ";
+    cin >> opcionOrganizador;
+
+    // Agrega el código para manejar las opciones del menú del organizador
+    switch (opcionOrganizador) {
+        case 1:
+            cout << "Seleccionaste: Crear actividad académica" << endl;
+            // Agrega el código para crear actividad académica
+            break;
+
+        case 2:
+            cout << "Seleccionaste: Eliminar actividad académica" << endl;
+            // Agrega el código para eliminar actividad académica
+            break;
+
+        // ... Agrega más casos según tus necesidades ...
+
+        default:
+            cout << "Opción no válida. Por favor, seleccione una opción válida." << endl;
+            break;
+    }
+}
+
+void menuDirectorAcademico() {
+    int opcionDirector;
+
+    cout << "Menú Director Académico:" << endl;
+    cout << "1. Enviar mailing" << endl;
+    cout << "2. Ver actividades académicas disponibles" << endl;
+    cout << "Ingrese el número correspondiente a su elección: ";
+    cin >> opcionDirector;
+
+    // Agrega el código para manejar las opciones del menú del director académico
+    switch (opcionDirector) {
+        case 1:
+            cout << "Seleccionaste: Enviar mailing" << endl;
+            // Agrega el código para enviar mailing
+            break;
+
+        case 2:
+            cout << "Seleccionaste: Ver actividades académicas disponibles" << endl;
+            // Agrega el código para ver actividades académicas disponibles
+            break;
+
+        default:
+            cout << "Opción no válida. Por favor, seleccione una opción válida." << endl;
+            break;
+    }
+}
+
+void menuUsuario() {
+    int opcionUsuario;
+
+    cout << "Menú Usuario:" << endl;
+    cout << "1. Inscribirse en una actividad" << endl;
+    cout << "2. Cancelar inscripción" << endl;
+    cout << "3. Ver actividades disponibles" << endl;
+    cout << "Ingrese el número correspondiente a su elección: ";
+    cin >> opcionUsuario;
+
+    // Agrega el código para manejar las opciones del menú del usuario
+    switch (opcionUsuario) {
+        case 1:
+            cout << "Seleccionaste: Inscribirse en una actividad" << endl;
+            // Agrega el código para inscribirse en una actividad
+            break;
+
+        case 2:
+            cout << "Seleccionaste: Cancelar inscripción" << endl;
+            // Agrega el código para cancelar inscripción
+            break;
+
+        case 3:
+            cout << "Seleccionaste: Ver actividades disponibles" << endl;
+            // Agrega el código para ver actividades disponibles
+            break;
+
+        default:
+            cout << "Opción no válida. Por favor, seleccione una opción válida." << endl;
+            break;
+    }
+}
+
+int main() {
+    int opcion;
+
+    cout << "Seleccione su rol:" << endl;
+    cout << "1. Organizador" << endl;
+    cout << "2. Director Academico" << endl;
+    cout << "3. Usuario" << endl;
+    cout << "Ingrese el número correspondiente a su elección: ";
+    cin >> opcion;
+
+    switch (opcion) {
+        case 1:
+            cout << "¡Bienvenido, Organizador!" << endl;
+            menuOrganizador();
+            break;
+
+        case 2:
+            cout << "¡Hola, Director Academico!" << endl;
+            menuDirectorAcademico();
+            break;
+
+        case 3:
+            cout << "¡Hola, Usuario!" << endl;
+            menuUsuario();
+            break;
+
+        default:
+            cout << "Opción no válida. Por favor, seleccione una opción válida." << endl;
+            break;
+    }
+
+    return 0;
+}
+
